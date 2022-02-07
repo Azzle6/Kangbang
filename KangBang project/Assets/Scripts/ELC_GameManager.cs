1bee1f8344fabffcbd1c0009fbc8c0076017e43e
Azzle <eliott.chauchard@orange.fr>
2022-02-07 15:18:26 +0100

    Score mieux en fait

--- KangBang project/Assets/Scripts/ELC_GameManager.cs
+++ KangBang project/Assets/Scripts/ELC_GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
@@ -12,12 +13,16 @@ public class ELC_GameManager : MonoBehaviour
 
     [HideInInspector] public int CurrentScore;
     [HideInInspector] public int CurrentMoney;
+    
     [SerializeField] private GeneralSettings SettingsSO;
+    
     public GameObject PlayerPrefab;
     public GameObject PlayerSpawnPosition;
     public List<SpikesZone> Spikes = new List<SpikesZone>(); //Mettre celui de droite en 1er et celui de gauche en 2ème position
     public GameObject ScoreGO;
 
+    public int CurrentPhase;
+    
     private GameObject Player;
 
     private void Awake()
@@ -50,5 +55,18 @@ public class ELC_GameManager : MonoBehaviour
         Destroy(Player);
         CurrentScore = 0;
     }
-    
+
+    public void TouchWall()
+    {
+        ELC_Move moveScript =Player.GetComponent<ELC_Move>();
+        moveScript.SwitchSide();
+        AddScore();
+        CurrentPhase++;
+        
+        if (moveScript.movingSide == 1)
+        {
+            Spikes[0].GenerateSpikes(SettingsSO.LevelsSettings[0].SpikesMinNumbers);
+        }
+        else Spikes[1].GenerateSpikes(SettingsSO.LevelsSettings[0].SpikesMinNumbers);
+    }
 }
